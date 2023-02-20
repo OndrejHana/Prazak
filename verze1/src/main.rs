335eea4294d2ef42c50ab8ff37860f8645aad8c3
OndrejHana <ondrahana001@gmail.com>
2023-02-20 09:22:02 +0100

    added cli arguments to version 1

--- verze1/src/main.rs
+++ verze1/src/main.rs
@@ -1,8 +1,19 @@
 use std::time;
 use graph::Graph;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// Number of cities(vertices) in a graph(it is recommended not to go above at most 20)
+    city_count: usize,
+
+    /// the upper bound value for weight of nodes. Lower bound will be calculated to preserve triangular inequity 
+    max_line_weight: usize,
+}
 
 fn main() {
-    let g = Graph::new(10, 100);
+    let args = Args::parse();
+    let g = Graph::new(args.city_count ,args.max_line_weight);
     let now = time::Instant::now();
     let (shortest_path_weight, path) =  g.find_shortest_path_brute_force(0);
     let elapsed = now.elapsed();
