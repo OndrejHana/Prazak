1c5b164fed36c80832db602771fcf6a68dc1f344
OndrejHana <ondrahana001@gmail.com>
2023-02-26 13:44:38 +0100

    fixed bug where the cost of shortest path was not calculated properly

--- verze1/src/lib.rs
+++ verze1/src/lib.rs
@@ -9,14 +9,20 @@ pub fn solve_tsp_brute_force(graph: &Graph, start: usize) -> (usize, Vec<usize>)
     walk(graph, start, &mut path, &mut shortest_path, &mut seen);
 
     let mut shortest_path_cost = 0;
-    for i in 0..graph.matrix.len()-1 {
-        shortest_path_cost += graph.matrix[shortest_path[i]][shortest_path[i+1]];
+    for i in 0..shortest_path.len() - 1 {
+        shortest_path_cost += graph.matrix[shortest_path[i]][shortest_path[i + 1]];
     }
 
     return (shortest_path_cost, shortest_path);
 }
 
-fn walk(graph: &Graph, current: usize, path: &mut Vec<usize>, shortest_path: &mut Vec<usize>, seen: &mut Vec<bool>) -> bool {
+fn walk(
+    graph: &Graph,
+    current: usize,
+    path: &mut Vec<usize>,
+    shortest_path: &mut Vec<usize>,
+    seen: &mut Vec<bool>,
+) -> bool {
     let city_count = graph.matrix.len();
 
     // base case
@@ -29,9 +35,9 @@ fn walk(graph: &Graph, current: usize, path: &mut Vec<usize>, shortest_path: &mu
 
         let mut shortest_path_cost = 0;
         let mut path_cost = 0;
-        for i in 0..graph.matrix.len()-1 {
-            shortest_path_cost += graph.matrix[shortest_path[i]][shortest_path[i+1]];
-            path_cost += graph.matrix[path[i]][path[i+1]];
+        for i in 0..graph.matrix.len() - 1 {
+            shortest_path_cost += graph.matrix[shortest_path[i]][shortest_path[i + 1]];
+            path_cost += graph.matrix[path[i]][path[i + 1]];
         }
 
         if path_cost < shortest_path_cost {
