89ca9abb654e32c2405207c94440dbeb65f5dfa5
OndrejHana <ondrahana001@gmail.com>
2023-02-27 05:14:58 +0100

    verze4

--- verze4_master/src/main.rs
+++ verze4_master/src/main.rs
@@ -1,3 +1,57 @@
+use std::io::{prelude::*, BufReader, BufWriter};
+use std::net::IpAddr;
+
+use config::Config;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Slave {
+    ip: IpAddr,
+    port: u16,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Settings {
+    port: u16,
+    slaves: Vec<Slave>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::builder()
+        .add_source(config::File::with_name("./master_config"))
+        .build()
+        .unwrap()
+        .try_deserialize::<Settings>()
+        .unwrap();
+
+    let mut slaves = Vec::new();
+    for slave in config.slaves {
+        slaves.push(
+            std::net::TcpStream::connect(format!("{}:{}", slave.ip, slave.port))
+                .expect("could not connect to configured slave"),
+        );
+    }
+
+    let mut computes = 0;
+
+    for slave in slaves.iter() {
+        let mut reader = BufReader::new(slave.clone());
+        let mut writer = BufWriter::new(slave);
+
+        writer.write_all(b"GET_COMPUTES\n").unwrap();
+        println!("sent!");
+
+        let mut buf = vec![0];
+        let mut bytes_read = 0;
+        while bytes_read == 0 {
+            bytes_read = reader.read_to_end(&mut buf).unwrap();
+        }
+        println!("{buf:?}");
+    }
+
+    if computes == 0 {
+        panic!();
+    }
+
+    println!("{computes}");
 }
--- verze4_slave/src/main.rs
+++ verze4_slave/src/main.rs
@@ -1,3 +1,44 @@
+use std::io::{prelude::*, BufReader, BufWriter};
+use std::net::TcpListener;
+
+use config::Config;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Settings {
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let config = Config::builder()
+        .add_source(config::File::with_name("./slave_config"))
+        .build()
+        .unwrap()
+        .try_deserialize::<Settings>()
+        .unwrap();
+
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", config.port)).unwrap();
+
+    for stream in listener.incoming() {
+        let mut master = stream.unwrap();
+        let mut reader = BufReader::new(&master);
+        let mut writer = BufWriter::new(&master);
+
+        let mut bytes_read;
+        let mut buf = vec![0u8; 32];
+        loop {
+            bytes_read = reader.read(&mut buf).unwrap();
+
+            if bytes_read != 0 {
+                break;
+            }
+        }
+
+        let str = std::str::from_utf8(&buf[..bytes_read]).unwrap();
+        if str.contains("GET_COMPUTES") {
+            let buf = vec![num_cpus::get() as u8];
+            writer.write(&buf).unwrap();
+        }
+        println!("Hello, world!");
+    }
 }
